b5100717b44b5de2dc7af15b6a6fd7c636ede7dd
Berin Aniesh <berinaniesh@gmail.com>
2023-10-10 15:48:42 +0530

    update year of kjv

--- sql/latest_data_only.sql
+++ sql/latest_data_only.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67893efba6ef5ec918d7cdc17de5dddc267bc0b954b671a64b22f463c0564c00
-size 38633430
+oid sha256:437937de5f553b5ab2ff327d20a87fe5fc2651ad37dc45039b9be1246dafcf66
+size 38633416
--- sql/latest_full.sql
+++ sql/latest_full.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ca29d6b3eec31440468db3ea3c84d5b375444f31a9615f1e08a01db41de0173
-size 28496095
+oid sha256:3751f183848b4f525ec488d1f04ac963f6f511b551df2fa2c159aef6cbabe073
+size 28496081
