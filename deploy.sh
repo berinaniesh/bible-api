65ef1c63d24f24d1629ec9634424c9396bb14ac6
Berin Aniesh <berinaniesh@gmail.com>
2023-10-10 15:44:31 +0530

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ ssh tncars.pp.ua cp /srv/bible-api/bible-api /srv/bible-api/bible-api.old
 ssh tncars.pp.ua sudo systemctl stop bible-api
 ssh tncars.pp.ua cp /srv/bible-api/bible-api.new /srv/bible-api/bible-api
 ssh tncars.pp.ua sudo systemctl start bible-api
+ssh tncars.pp.ua rm /srv/bible-api/bible-api.new
