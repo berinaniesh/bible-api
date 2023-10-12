66418ff0b669e5653648e3166571c8d0f14d3bde
Berin Aniesh <berinaniesh@gmail.com>
2023-10-12 08:47:10 +0530

    add asv

--- sql/latest_data_only.sql
+++ sql/latest_data_only.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:437937de5f553b5ab2ff327d20a87fe5fc2651ad37dc45039b9be1246dafcf66
-size 38633416
+oid sha256:8ac83df127e564723be4f56629163e713d55adc030db0dd20e026022146617f8
+size 45868447
--- sql/latest_full.sql
+++ sql/latest_full.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3751f183848b4f525ec488d1f04ac963f6f511b551df2fa2c159aef6cbabe073
-size 28496081
+oid sha256:f61e3374082e98740cb1b7b6e75f3cfb4b5982ddc26a7de129faa126d89ab306
+size 33104824
