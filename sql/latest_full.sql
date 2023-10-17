b4775a00ff996fd2239835856ebc427026074a34
Berin Aniesh <berinaniesh@gmail.com>
2023-10-17 11:16:12 +0530

    fix few verses

--- sql/latest_full.sql
+++ sql/latest_full.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f61e3374082e98740cb1b7b6e75f3cfb4b5982ddc26a7de129faa126d89ab306
-size 33104824
+oid sha256:a16bf3884f5ecabbd350c5a4e61557e89cbbf07498428ba31157349698069473
+size 33105060
