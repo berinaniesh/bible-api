d38db202172e2a5d2d783686b18262d6db55e5a0
Berin Aniesh <berinaniesh@gmail.com>
2024-04-03 12:24:54 +0530

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 cargo build --release
-scp target/release/bible-api tncars.pp.ua:/srv/bible-api/bible-api.new
-ssh tncars.pp.ua cp /srv/bible-api/bible-api /srv/bible-api/bible-api.old
-ssh tncars.pp.ua sudo systemctl stop bible-api
-ssh tncars.pp.ua cp /srv/bible-api/bible-api.new /srv/bible-api/bible-api
-ssh tncars.pp.ua sudo systemctl start bible-api
-ssh tncars.pp.ua rm /srv/bible-api/bible-api.new
+scp target/release/bible-api berinaniesh.xyz:/home/berinaniesh/tmp/
+ssh berinaniesh.xyz deploy-bible-api.sh
